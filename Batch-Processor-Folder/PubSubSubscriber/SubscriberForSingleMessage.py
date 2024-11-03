3401c827554052306b2bb09539ce1d278fa0443e
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2024-11-03 16:38:17 -0500

    changed folder structure

--- Batch-Processor-Folder/PubSubSubscriber/SubscriberForSingleMessage.py
+++ Batch-Processor-Folder/PubSubSubscriber/SubscriberForSingleMessage.py
@@ -48,5 +48,4 @@ def dequeue_one_message(project_id, subscription_id, timeout=10):
 # Replace with your project ID and subscription ID
 # project_id = "elated-scope-437703-h9"
 # subscription_id = "InputData-sub"
-dequeue_one_message("elated-scope-437703-h9", "InputData-su
-                    b")
+dequeue_one_message("elated-scope-437703-h9", "InputData-sub")
