1f9b57cf35b4784be1bb978810ada784055d3f61
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2024-11-14 02:09:32 -0500

    added new body parameters for the batch processor, currently commented out

--- Batch-Processor/function-http/main.py
+++ Batch-Processor/function-http/main.py
@@ -11,6 +11,15 @@ def go(request):
         data = request.get_json()  # Parse JSON data from request body
         print(data['Job_ID'])
         print(data['Client_ID'])
+        
+        # Can uncomment when we are ready
+        
+        # print(data['Project_ID'])
+        # print(data['Database_ID'])
+        # print(data['Input_Table_ID'])
+        # print(data['Output_Table_ID'])
+        # print(data['Model'])
+        
         # print(data['Database_Length'])
 
         # Call the handleGo function
