cdc5e096641cfd148ffcac45cb0c498a161251e0
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2024-11-14 01:52:54 -0500

    added hard coded values

--- Batch-Processor/function-http/pubSubSender.py
+++ Batch-Processor/function-http/pubSubSender.py
@@ -1,5 +1,14 @@
 from google.cloud import pubsub_v1 
 
+
+# Hard Coded Messages
+Project_ID =  "project_ID"
+Database_ID =  "database"
+Input_Table_ID =  "Input_table_ID"
+Output_Table_ID = "output_table_ID"
+Model =  "none"
+API-key =  "example_API_key"
+
 # Send message to Rate-Limtier via InputData Pub/Sub topic
 def pubSubSender(row, message, Job_ID, Client_ID):
     project_id = "elated-scope-437703-h9"
@@ -13,7 +22,13 @@ def pubSubSender(row, message, Job_ID, Client_ID):
     attributes = {
         "Job_ID": str(Job_ID),
         "Client_ID": str(Client_ID),
-        "Row_Number": str(row)
+        "Row_Number": str(row),
+        "Project_ID": Project_ID,
+        "Database_ID": Database_ID,
+        "Input_Table_ID": Input_Table_ID,
+        "Output_Table_ID": Output_Table_ID,
+        "Model": Model,
+        "API-key": API-key
     }
 
     # Send message to rate limiter
