a156320d86f9c475000a981ee195630690ae8829
Noah Robitshek <64294283+NoahRobit72@users.noreply.github.com>
2024-11-17 00:35:26 -0500

    added performance API

--- Performance/display.py
+++ Performance/display.py
@@ -2,31 +2,70 @@ import requests
 import time
 
 # Define the URL for the endpoints
-base_url = "http://localhost:8084"
+# base_url = "http://localhost:8084"
+base_url = "https://performance-api-1069651367433.us-central1.run.app"
 
 # Function to get batch processor count
 def get_batch_processor_load():
-    response = requests.get(f"{base_url}/getBatchProcessorLoad")
+
+    json_body = {"Job_ID": 1,"Microservice": "batch_processor"}
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.get(
+        f"{base_url}/getCount",
+        json=json_body,  # Add JSON body
+        headers=headers   # Add headers
+    )
+
     if response.status_code == 200:
-        return response.json().get("batch_processor_count", "N/A")
+        # print(response.json())
+        # print(getattr(response.json(), ("batch_processor")))
+        return response.json().get("batch_processor_count")
     return "Error"
 
 def get_total_rows():
-    response = requests.get(f"{base_url}/getTotalRows")
+    
+    json_body = {"Job_ID": 1,"Microservice": "total"}
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.get(
+        f"{base_url}/getCount",
+        json=json_body,  # Add JSON body
+        headers=headers   # Add headers
+    )
+    
     if response.status_code == 200:
         return response.json().get("total_count", "N/A")
     return "Error"
 
 # Function to get rate limiter count
 def get_rate_limiter_load():
-    response = requests.get(f"{base_url}/getRateLimiterLoad")
+    
+    json_body = {"Job_ID": 1,"Microservice": "rate_limiter"}
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.get(
+        f"{base_url}/getCount",
+        json=json_body,  # Add JSON body
+        headers=headers   # Add headers
+    )
+    
     if response.status_code == 200:
         return response.json().get("rate_limiter_count", "N/A")
     return "Error"
 
 # Function to get reverse batch processor count
 def get_reverse_batch_processor_load():
-    response = requests.get(f"{base_url}/getReverseBatchProcessorLoad")
+    
+    json_body = {"Job_ID": 1,"Microservice": "reverse_batch_processor"}
+    headers = {"Content-Type": "application/json"}
+
+    response = requests.get(
+        f"{base_url}/getCount",
+        json=json_body,  # Add JSON body
+        headers=headers   # Add headers
+    )
+    
     if response.status_code == 200:
         return response.json().get("reverse_batch_processor_count", "N/A")
     return "Error"
@@ -41,7 +80,6 @@ try:
         rate_limiter_count = get_rate_limiter_load()
         reverse_batch_processor_count = get_reverse_batch_processor_load()
         total_rows = get_total_rows()
-
 
         
         # Clear the console (works in most terminal environments)
