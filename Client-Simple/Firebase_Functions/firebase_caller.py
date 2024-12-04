7f5e22c2041e63fc8a85be6aa70a8f3542ed08cc
Rayan Syed <rayansyed@icloud.com>
2024-12-04 02:30:15 -0500

    cleanup

--- Client-Simple/Firebase_Functions/firebase_caller.py
+++
@@ -1,9 +0,0 @@
-from firebase_helper_files import getProgress
-
-Job_ID = "1"
-Client_ID = "Rick Sorkin"
-
-progressData = getProgress(Job_ID, Client_ID)
-
-print("current_row: " + str(progressData["current_row"]))
-print("total_row: " + str(progressData["total_rows"]))
--- rate-limit/container/RedisClient.py
+++
@@ -1,7 +0,0 @@
-'''Setup Redis Client'''
-import redis
-
-# Initialize Redis connection 
-redis_client = redis.Redis(host='10.84.53.171', port=6379, db=0)
-GLOBAL_BUCKET_KEY = "global_token_bucket"
-REQUEST_KEY = "request_limiter"
