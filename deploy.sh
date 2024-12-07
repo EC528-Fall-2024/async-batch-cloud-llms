3fe4d0a746687b2040efd7be35bda1dcb5411f9b
Rayan Syed <rayansyed@icloud.com>
2024-12-07 18:37:27 -0500

    deploy script refactor

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+# Redeploy all microservices from single script
 sh API/deploy.sh
 sh Job-Orchestrator/function/deploy.sh
 sh Batch-Processor/function/deploy.sh
@@ -5,4 +6,5 @@ sh rate-limit/function/deploy.sh
 sh Reverse-Batch-Processor/function/deploy.sh
 sh Stats-Collector/function/deploy.sh
 sh Status-Writer/function/deploy.sh
+
 echo "All Services Deployed"
